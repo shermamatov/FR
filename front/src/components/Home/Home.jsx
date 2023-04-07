47e5b5996e7f162c0717508708a82fd6a8f2be1a
alexander <itek@gmail.com>
2023-04-08 00:05:22 +0600

    new blocks

--- front/src/components/Home/Home.jsx
+++ front/src/components/Home/Home.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import FirstBlock from "./Blocks/Block1/FirstBlock";
 import FrServer from "./Blocks/Block4/FrServer";
 import WordsFrom from "./Blocks/Block5/WordsFrom";
-import LatestPost from "./Blocks/Block6/LatestPost";
-import ProjectLocation from "./Blocks/Block7/ProjectLocation";
 import Block2 from "./Blocks/Block2/Block2";
 import Block3 from "./Blocks/Block3/Block3";
+import Block6 from "./Blocks/Block6/Block6";
+import Block7 from "./Blocks/Block7/Block7";
 // block end
 // строго использовать css.module
 const Home = () => {
@@ -17,8 +17,8 @@ const Home = () => {
             <Block3 />
             <FrServer />
             <WordsFrom />
-            <LatestPost />
-            <ProjectLocation />
+            <Block6 />
+            <Block7 />
         </div>
     );
 };
---
+++ front/src/const/consts.js
@@ -0,0 +1,113 @@
+import img from "../images/block7_img2.webp";
+export const otzv1 = [
+    {
+        name: "Jane Cooper",
+        desc: "My second time using them. They are quick to respond, very nice, and have reasonable prices. I will definitely be calling them in the future with any of my home hvac needs/repairs.",
+        id: 1,
+        rating: "4.5",
+    },
+    {
+        name: "Eleanor Pena",
+        desc: "My second time using them. They are quick to respond, very nice, and have reasonable prices. I will definitely be calling them in the future with any of my home hvac needs/repairs.",
+        id: 2,
+        rating: "4.3",
+    },
+    {
+        name: "Arlene McCoy",
+        desc: "My second time using them. They are quick to respond, very nice, and have reasonable prices. I will definitely be calling them in the future with any of my home hvac needs/repairs.",
+        id: 3,
+        rating: "4.9",
+    },
+    {
+        name: "Savannah Nguyen",
+        desc: "My second time using them. They are quick to respond, very nice, and have reasonable prices. I will definitely be calling them in the future with any of my home hvac needs/repairs.",
+        id: 4,
+        rating: "5",
+    },
+    {
+        name: "Jane Cooper",
+        desc: "My second time using them. They are quick to respond, very nice, and have reasonable prices. I will definitely be calling them in the future with any of my home hvac needs/repairs.",
+        id: 5,
+        rating: "4.4",
+    },
+    {
+        name: "Savannah Nguyen",
+        desc: "My second time using them. They are quick to respond, very nice, and have reasonable prices. I will definitely be calling them in the future with any of my home hvac needs/repairs.",
+        id: 6,
+        rating: "4.0",
+    },
+    {
+        name: "Arlene Eleanor",
+        desc: "My second time using them. They are quick to respond, very nice, and have reasonable prices. I will definitely be calling them in the future with any of my home hvac needs/repairs.",
+        id: 7,
+        rating: "5",
+    },
+    // {
+    //     name: "Arlene Eleanor",
+    //     desc: "My second time using them. They are quick to respond, very nice, and have reasonable prices. I will definitely be calling them in the future with any of my home hvac needs/repairs.",
+    //     id: 8,
+    //     rating: "5",
+    // },
+    // {
+    //     name: "Arlene Eleanor",
+    //     desc: "My second time using them. They are quick to respond, very nice, and have reasonable prices. I will definitely be calling them in the future with any of my home hvac needs/repairs.",
+    //     id: 9,
+    //     rating: "5",
+    // },
+];
+export const otzv2 = [
+    {
+        name: "Jane Cooper",
+        img,
+        id: 1,
+        rating: "4.5",
+    },
+    {
+        name: "Eleanor Pena",
+        img,
+        id: 2,
+        rating: "4.3",
+    },
+    {
+        name: "Arlene McCoy",
+        img,
+        id: 3,
+        rating: "4.9",
+    },
+    {
+        name: "Savannah Nguyen",
+        img,
+        id: 4,
+        rating: "5",
+    },
+    {
+        name: "Jane Cooper",
+        img,
+        id: 5,
+        rating: "4.4",
+    },
+    {
+        name: "Savannah Nguyen",
+        img,
+        id: 6,
+        rating: "4.0",
+    },
+    {
+        name: "Arlene Eleanor",
+        img,
+        id: 7,
+        rating: "5",
+    },
+    // {
+    //     name: "Arlene Eleanor",
+    //     desc: "My second time using them. They are quick to respond, very nice, and have reasonable prices. I will definitely be calling them in the future with any of my home hvac needs/repairs.",
+    //     id: 8,
+    //     rating: "5",
+    // },
+    // {
+    //     name: "Arlene Eleanor",
+    //     desc: "My second time using them. They are quick to respond, very nice, and have reasonable prices. I will definitely be calling them in the future with any of my home hvac needs/repairs.",
+    //     id: 9,
+    //     rating: "5",
+    // },
+];
