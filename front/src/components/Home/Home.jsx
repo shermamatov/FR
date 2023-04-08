a46b49ce7082921dd7e2ab37ab7752585681c6b5
alexander <itek@gmail.com>
2023-04-09 01:18:43 +0600

    homepage

--- front/src/components/Home/Home.jsx
+++ front/src/components/Home/Home.jsx
@@ -5,8 +5,9 @@ import FrServer from "./Blocks/Block4/FrServer";
 import WordsFrom from "./Blocks/Block5/WordsFrom";
 import Block2 from "./Blocks/Block2/Block2";
 import Block3 from "./Blocks/Block3/Block3";
-import Block6 from "./Blocks/Block6/Block6";
+// import Block6 from "./Blocks/Block6/Block6";
 import Block7 from "./Blocks/Block7/Block7";
+import LatestPost from "./Blocks/Block6/LatestPost";
 // block end
 // строго использовать css.module
 const Home = () => {
@@ -16,9 +17,9 @@ const Home = () => {
             <Block2 />
             <Block3 />
             <FrServer />
-            {/* <WordsFrom /> */}
-            <Block6 />
+            <WordsFrom />
             <Block7 />
+            <LatestPost />
         </div>
     );
 };
