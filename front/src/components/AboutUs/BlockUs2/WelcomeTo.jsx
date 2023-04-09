e950be0de1dea63fb103d2b0cec3ca5d6c3875d5
alexander <itek@gmail.com>
2023-04-10 00:48:59 +0600

    fix bugs with hige windows

--- front/src/components/AboutUs/BlockUs2/WelcomeTo.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const WelcomeTo = () => {
-    return <div>WelcomeTo</div>;
-};
-
-export default WelcomeTo;
--- front/src/components/AboutUs/BlockUs3/Location.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Location = () => {
-    return <div>locatin</div>;
-};
-
-export default Location;
