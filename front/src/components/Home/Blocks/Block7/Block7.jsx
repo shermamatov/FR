7baf21dfbd5ba4293c610e40cd2c7de478e46533
alexander <itek@gmail.com>
2023-04-10 05:23:24 +0600

    add new slider

--- front/src/components/Home/Blocks/Block7/Block7.jsx
+++ front/src/components/Home/Blocks/Block7/Block7.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import Slider from 'react-slick';
 import "./block7.scss";
 import { otzv1, otzv2 } from "../../../../const/consts";
 import rating from "../../../../images/block7_rating.png";
@@ -7,6 +8,37 @@ import img2 from "../../../../images/block7_img2.webp";
 import img3 from "../../../../images/block7_img3.webp";
 import { useChel } from "../../../../Contexts/ChelContext";
 const Block7 = () => {
+    const settings = {
+        arrows:false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 1208,
+                settings: {
+                  slidesToShow: 2, // на экранах меньше 768px показываем 2 слайда
+                },
+              },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1, // на экранах меньше 768px показываем 2 слайда
+              },
+            },
+          ],
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        swipe: true,
+        swipeToSlide: true,
+        touchMove: true,
+        pauseOnHover: true,
+        onContextMenu: (e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          return false;
+        }
+      };      
     const { addBordClass } = useChel();
     useEffect(() => {
         addBordClass();
@@ -15,28 +47,32 @@ const Block7 = () => {
         <div className="block7_main">
             <h1>Words From Customers</h1>
             <div className="text_review">
-                {otzv1.map((item) => (
-                    <div className="text_review_block" key={item.id}>
-                        <p>{item.desc}</p>
-                        <h5>{item.name}</h5>
-                        <div>
-                            <img src={rating} alt="" />
-                            <p>{item.rating}</p>
+                <Slider {...settings}>
+                    {otzv1.map((item) => (
+                        <div className="text_review_block" key={item.id}>
+                            <p>{item.desc}</p>
+                            <h5>{item.name}</h5>
+                            <div>
+                                <img src={rating} alt="" />
+                                <p>{item.rating}</p>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </Slider>
             </div>
-            <div className="video_review_adap">
-                {otzv2.map((item) => (
-                    <div className="video_review_block_adap" key={item.id}>
-                        <img src={item.img} alt="" />
-                        <h5>{item.name}</h5>
-                        <div>
-                            <img src={rating} alt="" />
-                            <p>{item.rating}</p>
+            <div  className="video_review_adap">
+                <Slider {...settings}>
+                    {otzv2.map((item) => (
+                        <div className="video_review_block_adap" key={item.id}>
+                            <img src={item.img} alt="" />
+                            <h5>{item.name}</h5>
+                            <div>
+                                <img src={rating} alt="" />
+                                <p>{item.rating}</p>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </Slider>
             </div>
             <div className="video_review">
                 <div className="video_left">
---
+++ front/src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import './privacypolicy.css'
+
+export default function PrivacyPolicy() {
+  return (
+    <div>
+        <div id="inner_content-172-20" class="container_pol">
+        <div>
+        <h1 class="">Privacy Policy</h1>
+        <p>
+            <span>
+            Through advanced AI technology and well tested methods we can provide unique websites that will ensure the fast development and success of your business. With over 30+ years of experience in AI and computer science, unique methodology of creating and promoting sites, we are confident that we won’t let you down. Choose #1 1Furniture restoration - we care about you and your future.								
+            </span>
+        </p>
+        <h2><strong>Who We Are</strong></h2>
+        <p>
+        <span>
+        For over 3 generations members of our family have been IT professionals and engineers. The absolute passion and love towards technology became the reason for establishing #1 1Furniture restoration.com. We have developed our own methods for creating and promoting websites. Knowing that AI is of particular importance in the modern world, our business is mainly concentrated on actively using AI technology in business. Our goal is to make sure our methods help you be successful!</span></p>
+        <h2><strong>Visitor Comments</strong></h2>
+        <p>
+        <span>
+            When a visitor submits a comment on our website, we collect all the data provided in the comments form. Our system also records the visitor’s IP address and browser user agent string. This enables us to detect spam and other potentially malicious activities. We also generate an anonymized string from your email address. Also known as a hash, it may be provided to the Gravatar service, which allows the publishing of information across the open web. Once a comment is approved, your profile picture will be visible to the public in a relevant context.&nbsp;</span></p>
+        <p>
+        <span>
+            *To view the Gravatar service privacy policy, visit </span><a href="https://automattic.com/privacy/"><span>
+            https://automattic.com/privacy/</span></a><span>
+            .</span></p>
+        <h2><strong>Media</strong></h2>
+        <p>
+        <span>
+            When uploading images to this website, avoid uploading them with embedded location data (EXIF GPS). Visitors to this website can access images and download and extract location data from any image that has been uploaded.</span></p>
+        <h2><strong>Cookies</strong></h2>
+        <p>
+        <span>
+            Cookies are bits of information that include your name, email address, and website. They are saved for one year. You have the option to have your cookies saved when leaving a comment on this website. By permitting us to save this information, you won’t need to fill in login details again when leaving another comment.&nbsp;</span></p>
+        <p>
+        <span>
+            When you use our login page for the first time, a temporary cookie will be set up. This will determine if your web browser accepts cookies. It contains no personal data and will be deleted once you close your browser.&nbsp;</span></p>
+        <p>
+        <span>
+            Cookies saved when you log in to our comment system include your login details and screen display choices. You have the option to select “Remember Me”; if you select it, your login will remain valid for two weeks (unless you log out of your account, in which case the cookies will be removed). Otherwise, login cookies last for two days, and screen options cookies last for a year.&nbsp;</span></p>
+        <p>
+        <span>
+            An additional cookie is saved in your browser when you edit or publish an article. It only contains the post ID of the article you edited, and does not contain any personal data. This cookie is removed after 1 day.</span></p>
+        <h2><strong>Embedded Content from Other Websites</strong></h2>
+        <p>
+        <span>
+            The articles we include on this website may contain embedded content, including images, videos, and articles, that originate from other websites. This embedded content behaves the same as if the viewer is visiting the source website.&nbsp;</span></p>
+        <p>
+        <span>
+            Such a website may collect data about you. They may also use cookies, monitor your interaction with its content, and embed third-party tracking tools. Your interactions with embedded content can be tracked if you have an account with the source website and are logged in to it.</span></p>
+        <h2><strong>Our Data-Sharing Policy</strong></h2>
+        <p>
+        <span>
+            We include your IP address in any email sent in response to a password reset request.&nbsp;</span></p>
+        <p>
+        <span>
+            Submitted comments and their metadata are retained for an indefinite amount of time. This avoids having to retain comments in a moderation queue; instead, we can recognize and approve any follow-up comments automatically.</span></p>
+        <p>
+        <span>
+            We store the personal information of users registered on this website. The personal data we store include the details of your user profile. You cannot change your username, but you can see, edit, and delete your personal information. Website administrators also have the authority to see and edit any information you provide us.</span></p>
+        <p>
+        <span>
+            We may also run visitor comments through an automated spam detection service to determine their authenticity.</span></p>
+        <h2><strong>Your Rights Over Your Data</strong></h2>
+        <p>
+        <span>
+            Account holders on this site, and individuals who have left commons, can request an exported file of the personal data we hold. Any data you provided us will be included in this file. Aside from the data we keep for administrative, security, or legal purposes, you can request that we erase any personal data we hold about you.</span></p>
+        <h2><strong>Contact Us</strong></h2>
+        <p>
+        <span>
+            If you have any questions about our privacy policy, how we use your data, or the HVAC and plumbing services we provide in Southern California, call us at <a href="tel:+1(818)8581415">+1(818)8581415</a>.</span></p>
+        </div>
+        </div>
+        
+    </div>
+  )
+}
