10f42269ae34dc0047164ecaf1934deef3360050
unknown <timurshermamatov1@gmail.com>
2023-04-16 02:40:06 +0600

    add

--- front/src/Routes/MainRoutes.js
+++ front/src/Routes/MainRoutes.js
@@ -3,21 +3,27 @@ import { Route, Routes } from "react-router-dom";
 import AboutUsPage from "../Pages/AboutUsPage";
 import HomePage from "../Pages/HomePage";
 import PrivacyPolicyPage from "../Pages/PrivacyPolicyPage";
+import ServicesPage from "../Pages/ServicesPage";
 const MainRoutes = () => {
-  const PUBLIC_ROUTES = [
-    { link: "/AboutUs", Element: <AboutUsPage />, id: 1 },
-    { link: "/", Element: <HomePage />, id: 2 },
-    { link: "/privacy_policy", Element: <PrivacyPolicyPage />, id: 3 },
-  ];
-  return (
-    <>
-      <Routes>
-        {PUBLIC_ROUTES.map((item) => (
-          <Route path={item.link} element={item.Element} key={item.id} />
-        ))}
-      </Routes>
-    </>
-  );
+    const PUBLIC_ROUTES = [
+        { link: "/AboutUs", Element: <AboutUsPage />, id: 1 },
+        { link: "/", Element: <HomePage />, id: 2 },
+        { link: "/privacy_policy", Element: <PrivacyPolicyPage />, id: 3 },
+        { link: "/services", Element: <ServicesPage />, id: 4 },
+    ];
+    return (
+        <>
+            <Routes>
+                {PUBLIC_ROUTES.map((item) => (
+                    <Route
+                        path={item.link}
+                        element={item.Element}
+                        key={item.id}
+                    />
+                ))}
+            </Routes>
+        </>
+    );
 };
 
 export default MainRoutes;
