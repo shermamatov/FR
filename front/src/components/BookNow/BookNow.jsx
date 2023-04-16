ab68243bdc725aadf9e1d91ea6979908b853359d
alexander <itybek@gmail.com>
2023-04-17 04:55:11 +0600

    asd

--- front/src/components/BookNow/BookNow.jsx
+++ front/src/components/BookNow/BookNow.jsx
@@ -1,9 +1,13 @@
 import BlockNow1 from "./BlockNow1/BlockNow1";
+import BlockNow2 from "./BlockNow2/BlockNow2";
 
 const BookNow = () => {
-  return <div>
-    <BlockNow1 />
-    </div>;
+  return (
+    <div>
+      <BlockNow1 />
+      <BlockNow2 />
+    </div>
+  );
 };
 
 export default BookNow;
