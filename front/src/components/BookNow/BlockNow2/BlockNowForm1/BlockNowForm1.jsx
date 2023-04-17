c4bdbce2b61c4223f60a023dc2fcabd4a740daa1
nazaraliev-kubanychbek <kuba.duishobaevich@gmail.com>
2023-04-17 07:24:26 +0600

    book now form

--- front/src/components/BookNow/BlockNow2/BlockNowForm1/BlockNowForm1.jsx
+++ front/src/components/BookNow/BlockNow2/BlockNowForm1/BlockNowForm1.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from "react";
 import "./BlockNowForm1.css";
 
-const BlockNow2 = ({ changeLocalStorage }) => {
+const BlockNowForm1 = ({ changeLocalStorage }) => {
     //   const [name, setName] = useState("");
     //   const [firstNmae, serFirstName] = useState("");
     //   const [number, setNumber] = useState("");
     return (
         <>
-            <div
+            <div className="blockNowForm1"
             // style={{ display: "flex" }}
             >
-                <h1>meeting</h1>
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-                <button onClick={() => changeLocalStorage(false)}>
+                <h1 className="blockNowForm1_title">Fill out the form and we will come as soon as possible.</h1>
+                <input placeholder="Name" type="text" />
+                <input placeholder="Phone" type="tel" />
+                <input placeholder="E-Mail" type="text" />
+                <input placeholder="Address" type="text" />
+                <input placeholder="Service" type="text" />
+                <button onClick={() => changeLocalStorage(true)}>
                     {" "}
-                    button 1
+                   Next
                 </button>
             </div>
         </>
     );
 };
 
-export default BlockNow2;
+export default BlockNowForm1;
--- front/src/components/BookNow/BlockNow2/BlockNowForm2/BlockNowForm2.jsx
+++ front/src/components/BookNow/BlockNow2/BlockNowForm2/BlockNowForm2.jsx
@@ -1,93 +1,148 @@
 import React, { useState } from "react";
 import Slider from "react-slick";
 import "./BlockNowForm2.css";
-const BlockNowForm2 = ({ changeLocalStorage }) => {
-    const [day, setDay] = useState(3);
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-    };
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronRight,
+  faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons";
 
-    return (
-        <div>
-            <h2> Multiple items </h2>
-            <Slider {...settings}>
-                <div
-                    className="date_block"
-                    onClick={() => {
-                        setDay(20);
-                        console.log(day);
-                    }}
-                >
-                    <h2>ponedelnik</h2>
-                    <h1>20</h1>
-                </div>
-                <div
-                    className="date_block"
-                    onClick={() => {
-                        setDay(21);
-                        console.log(day);
-                    }}
-                >
-                    <h2>vtornik</h2>
-                    <h1>21</h1>
-                </div>
-                <div
-                    className="date_block"
-                    onClick={() => {
-                        setDay(22);
-                        console.log(day);
-                    }}
-                >
-                    <h2>sreda</h2>
-                    <h1>22</h1>
-                </div>
-                <div
-                    className="date_block"
-                    onClick={() => {
-                        setDay(23);
-                        console.log(day);
-                    }}
-                >
-                    <h2>chetverg</h2>
-                    <h1>23</h1>
-                </div>
-                <div
-                    className="date_block"
-                    onClick={() => {
-                        setDay(24);
-                        console.log(day);
-                    }}
-                >
-                    <h2>pyatnica</h2>
-                    <h1>24</h1>
-                </div>
-                <div
-                    className="date_block"
-                    onClick={() => {
-                        setDay(25);
-                        console.log(day);
-                    }}
-                >
-                    <h2>subota</h2>
-                    <h1>25</h1>
-                </div>
-                <div
-                    className="date_block"
-                    onClick={() => {
-                        setDay(26);
-                        console.log(day);
-                    }}
-                >
-                    <h2>voskresenie</h2>
-                    <h1>26</h1>
-                </div>
-            </Slider>
-        </div>
-    );
+const BlockNowForm2 = ({ changeLocalStorage }) => {
+  const [day, setDay] = useState(3);
+  const settings = {
+    // dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="blockNowForm2">
+        <p className="blockNowForm2_choose">Choose day and time</p>
+      <h2 className="blockNowForm2_month">
+        <button className="date_block-wrapper-prev-btn">
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>{" "}
+        February{" "}
+        <button className="date_block-wrapper-next-btn">
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
+      </h2>
+      <div className="date_block-wrapper">
+        <button className="date_block-wrapper-prev-btn">
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
+        <Slider {...settings}>
+          <div>
+            <div
+              className="date_block"
+              onClick={() => {
+                setDay(20);
+                console.log(day);
+              }}
+            >
+              <h2>pon</h2>
+              <h1>20</h1>
+            </div>
+          </div>
+
+          <div>
+            <div
+              className="date_block"
+              onClick={() => {
+                setDay(21);
+                console.log(day);
+              }}
+            >
+              <h2>vtornik</h2>
+              <h1>21</h1>
+            </div>
+          </div>
+
+          <div>
+            <div
+              className="date_block"
+              onClick={() => {
+                setDay(22);
+                console.log(day);
+              }}
+            >
+              <h2>sreda</h2>
+              <h1>22</h1>
+            </div>
+          </div>
+
+          <div>
+            <div
+              className="date_block"
+              onClick={() => {
+                setDay(23);
+                console.log(day);
+              }}
+            >
+              <h2>chetverg</h2>
+              <h1>23</h1>
+            </div>
+          </div>
+
+          <div>
+            <div
+              className="date_block"
+              onClick={() => {
+                setDay(24);
+                console.log(day);
+              }}
+            >
+              <h2>pyatnica</h2>
+              <h1>24</h1>
+            </div>
+          </div>
+
+          <div>
+            <div
+              className="date_block"
+              onClick={() => {
+                setDay(25);
+                console.log(day);
+              }}
+            >
+              <h2>subota</h2>
+              <h1>25</h1>
+            </div>
+          </div>
+
+          <div>
+            <div
+              className="date_block"
+              onClick={() => {
+                setDay(26);
+                console.log(day);
+              }}
+            >
+              <h2>voskr</h2>
+              <h1>26</h1>
+            </div>
+          </div>
+        </Slider>
+        <button className="date_block-wrapper-next-btn">
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
+      </div>
+
+      <h2 className="blockNowForm2_time">Select time for Monday, February 20</h2>
+      <div className="blockNowForm2_time_wrapper">
+        <p className="blockNowForm2_time_item">8 am - 11 am</p>
+        <p className="blockNowForm2_time_item">11 am - 2 pm</p>
+        <p className="blockNowForm2_time_item">2 pm - 5 pm</p>
+        <p className="blockNowForm2_time_item">5 pm - 8 pm</p>
+      </div>
+      <textarea placeholder="Comment" className="blockNowForm2_textarea"></textarea>
+      <button onClick={()=>{
+        changeLocalStorage(false)
+      }} className="blockNowForm2_btn">Book now</button>
+    </div>
+  );
 };
 
 export default BlockNowForm2;
