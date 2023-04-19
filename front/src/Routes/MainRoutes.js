41a07226b0c6ffca760ad26438e5ff3bcacedffc
alexander <itybek@gmail.com>
2023-04-19 15:33:07 +0600

    blogpage

--- front/src/Routes/MainRoutes.js
+++ front/src/Routes/MainRoutes.js
@@ -5,6 +5,8 @@ import HomePage from "../Pages/HomePage";
 import PrivacyPolicyPage from "../Pages/PrivacyPolicyPage";
 import BookNowPage from "../Pages/BookNowPage";
 import ServicesPage from "../Pages/ServicesPage";
+import Photos from "../components/Photos/Photos";
+import BlogPage from "../Pages/BlogPage";
 const MainRoutes = () => {
   const PUBLIC_ROUTES = [
     { link: "/AboutUs", Element: <AboutUsPage />, id: 1 },
@@ -12,6 +14,8 @@ const MainRoutes = () => {
     { link: "/privacy_policy", Element: <PrivacyPolicyPage />, id: 3 },
     { link: "/book_now", Element: <BookNowPage />, id: 4 },
     { link: "/services", Element: <ServicesPage />, id: 5 },
+    { link: "/photos", Element: <Photos />, id: 6 },
+    { link: "/blog", Element: <BlogPage />, id: 7 },
   ];
   return (
     <>
