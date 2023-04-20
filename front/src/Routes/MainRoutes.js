728681b720d95dab23075d0bfb1935360c67a54e
nazaraliev-kubanychbek <85123145+nazaraliev-kubanychbek@users.noreply.github.com>
2023-04-20 23:24:24 +0600

    Update MainRoutes.js

--- front/src/Routes/MainRoutes.js
+++ front/src/Routes/MainRoutes.js
@@ -5,15 +5,12 @@ import HomePage from "../Pages/HomePage";
 import PrivacyPolicyPage from "../Pages/PrivacyPolicyPage";
 import BookNowPage from "../Pages/BookNowPage";
 import ServicesPage from "../Pages/ServicesPage";
-<<<<<<< HEAD
 import ReviewsPage from "../Pages/ReviewsPage";
 import PhotosPage from "../Pages/PhotosPage";
-=======
-
 import Photos from "../components/Photos/Photos";
 import BlogPage from "../Pages/BlogPage";
 import Services2Page from "../Pages/Services2Page";
->>>>>>> 18c9475f38aeb4a30e7b854c208b13e804609bdc
+
 const MainRoutes = () => {
     const PUBLIC_ROUTES = [
         { link: "/AboutUs", Element: <AboutUsPage />, id: 1 },
@@ -21,14 +18,12 @@ const MainRoutes = () => {
         { link: "/privacy_policy", Element: <PrivacyPolicyPage />, id: 3 },
         { link: "/book_now", Element: <BookNowPage />, id: 4 },
         { link: "/services", Element: <ServicesPage />, id: 5 },
-<<<<<<< HEAD
         { link: "/photos", Element: <PhotosPage />, id: 6 },
         { link: "/reviews", Element: <ReviewsPage />, id: 7 },
-=======
-        { link: "/photos", Element: <Photos />, id: 6 },
-        { link: "/blog", Element: <BlogPage />, id: 7 },
-        { link: "/services2", Element: <Services2Page />, id: 8 },
->>>>>>> 18c9475f38aeb4a30e7b854c208b13e804609bdc
+        { link: "/photos", Element: <Photos />, id: 8 },
+        { link: "/blog", Element: <BlogPage />, id: 9 },
+        { link: "/services2", Element: <Services2Page />, id: 10 },
+
     ];
     return (
         <>
