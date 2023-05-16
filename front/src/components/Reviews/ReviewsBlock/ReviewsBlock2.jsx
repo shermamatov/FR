a2fb3a9783af6546f16698003639e8b7893f1149
unknown <timurshermamatov1@gmail.com>
2023-05-17 03:49:53 +0600

    front

--- front/src/components/Reviews/ReviewsBlock/ReviewsBlock2.jsx
+++ front/src/components/Reviews/ReviewsBlock/ReviewsBlock2.jsx
@@ -3,160 +3,177 @@ import reviewsBlock2Card from "../img/reviewsBlock2-card.png";
 import reviewsBlock2Stars from "../img/reviewsBlock2-stars.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faChevronRight,
-  faChevronLeft,
+    faChevronRight,
+    faChevronLeft,
 } from "@fortawesome/free-solid-svg-icons";
 import ReviewsFilterPopup from "./ReviewsPopup/ReviewsFilterPopup/ReviewsFilterPopup";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Rating from "@mui/material/Rating";
+import { styled } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { createNestedList, getReviews } from "../../../redux/reducers/app";
 
 const ReviewsBlock2 = () => {
-  const [showFilterPopup, setShowFilterPopup] = useState(false);
+    const [showFilterPopup, setShowFilterPopup] = useState(false);
+    const dispatch = useDispatch();
 
-  return (
-    <section className="reviewsBlock2">
-      <div className="container">
-        <div className="reviewsBlock2_btns">
-          <button className="reviewsBlock2_btns_filter"
-          onClick={()=>{
-            setShowFilterPopup(!showFilterPopup)
-          }}
-          >
-            <img src={reviewsBlock2Filter} alt="" />
-            Filter
-          </button>
-          <ReviewsFilterPopup showFilterPopup={showFilterPopup} />
-          <div className="reviewsBlock2_btns_right">
-            <button>All</button>
-            <button>Review</button>
-            <button>Video review</button>
-          </div>
-        </div>
-        <h1 className="reviewsBlock2_title">Video review</h1>
-        <div className="row">
-          <div className="col-3">
-            <div className="reviewsBlock2_card">
-              <button className="reviewsBlock2_card_btn">Play video</button>
-              <img
-                src={reviewsBlock2Card}
-                alt=""
-                className="reviewsBlock2_card_img"
-              />
-              <h3 className="reviewsBlock2_card_title">Jacob Jones</h3>
-              <div className="reviewsBlock2_card_raiting">
-                <div className="reviewsBlock2_card_raiting_icons">
-                  <img src={reviewsBlock2Stars} alt="" />
-                </div>
-                <p className="reviewsBlock2_card_raiting_count">4.5</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-3">
-            <div className="reviewsBlock2_card">
-              <button className="reviewsBlock2_card_btn">Play video</button>
-              <img
-                src={reviewsBlock2Card}
-                alt=""
-                className="reviewsBlock2_card_img"
-              />
-              <h3 className="reviewsBlock2_card_title">Jacob Jones</h3>
-              <div className="reviewsBlock2_card_raiting">
-                <div className="reviewsBlock2_card_raiting_icons">
-                  <img src={reviewsBlock2Stars} alt="" />
-                </div>
-                <p className="reviewsBlock2_card_raiting_count">4.5</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-3">
-            <div className="reviewsBlock2_card">
-              <button className="reviewsBlock2_card_btn">Play video</button>
-              <img
-                src={reviewsBlock2Card}
-                alt=""
-                className="reviewsBlock2_card_img"
-              />
-              <h3 className="reviewsBlock2_card_title">Jacob Jones</h3>
-              <div className="reviewsBlock2_card_raiting">
-                <div className="reviewsBlock2_card_raiting_icons">
-                  <img src={reviewsBlock2Stars} alt="" />
-                </div>
-                <p className="reviewsBlock2_card_raiting_count">4.5</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-3">
-            <div className="reviewsBlock2_card">
-              <button className="reviewsBlock2_card_btn">Play video</button>
-              <img
-                src={reviewsBlock2Card}
-                alt=""
-                className="reviewsBlock2_card_img"
-              />
-              <h3 className="reviewsBlock2_card_title">Jacob Jones</h3>
-              <div className="reviewsBlock2_card_raiting">
-                <div className="reviewsBlock2_card_raiting_icons">
-                  <img src={reviewsBlock2Stars} alt="" />
-                </div>
-                <p className="reviewsBlock2_card_raiting_count">4.5</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-3">
-            <div className="reviewsBlock2_card">
-              <button className="reviewsBlock2_card_btn">Play video</button>
-              <img
-                src={reviewsBlock2Card}
-                alt=""
-                className="reviewsBlock2_card_img"
-              />
-              <h3 className="reviewsBlock2_card_title">Jacob Jones</h3>
-              <div className="reviewsBlock2_card_raiting">
-                <div className="reviewsBlock2_card_raiting_icons">
-                  <img src={reviewsBlock2Stars} alt="" />
-                </div>
-                <p className="reviewsBlock2_card_raiting_count">4.5</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-3">
-            <div className="reviewsBlock2_card">
-              <button className="reviewsBlock2_card_btn">Play video</button>
-              <img
-                src={reviewsBlock2Card}
-                alt=""
-                className="reviewsBlock2_card_img"
-              />
-              <h3 className="reviewsBlock2_card_title">Jacob Jones</h3>
-              <div className="reviewsBlock2_card_raiting">
-                <div className="reviewsBlock2_card_raiting_icons">
-                  <img src={reviewsBlock2Stars} alt="" />
-                </div>
-                <p className="reviewsBlock2_card_raiting_count">4.5</p>
-              </div>
-            </div>
-          </div>
-        </div>
+    const reviewsArr = useSelector((s) => s.app.reviews);
 
-        <div className="reviewsBlock2_bottom_row">
-          <div className="reviewsBlock2_bottom">
-            <button className="reviewsBlock2_bottom_btn">
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </button>
-            <div className="reviewsBlock2_bottom_center">
-              <p className="reviewsBlock2_bottom_center_item">1</p>
-              <p className="reviewsBlock2_bottom_center_item">2</p>
-              <p className="reviewsBlock2_bottom_center_item">3</p>
-              <p className="reviewsBlock2_bottom_center_item">. . . . .</p>
-              <p className="reviewsBlock2_bottom_center_item">16</p>
+    const [paggArr, setPaggArr] = useState(
+        createNestedList(reviewsArr, 4) || []
+    );
+    const [reviewsState, setReviewsState] = useState(reviewsArr || []);
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const StyledRating = styled(Rating)({
+        "& .MuiRating-iconFilled": {
+            color: "#f2994a",
+        },
+        "& .MuiRating-iconHover": {
+            color: "#ff3d47",
+        },
+    });
+
+    function setPage(index) {
+        setReviewsState(paggArr[index]);
+    }
+
+    function nextPage() {
+        if (currentPage < paggArr.length - 1) {
+            setReviewsState(paggArr[currentPage + 1]);
+            setCurrentPage(currentPage + 1);
+            console.log(currentPage);
+        } else {
+            setReviewsState(paggArr[0]);
+            setCurrentPage(0);
+        }
+    }
+
+    function prevPage() {
+        if (currentPage > 0) {
+            setReviewsState(paggArr[currentPage - 1]);
+            setCurrentPage(currentPage - 1);
+        } else if (currentPage == 0) {
+            setReviewsState(paggArr[paggArr.length - 1]);
+            setCurrentPage(paggArr.length - 1);
+        }
+    }
+
+    useEffect(() => {
+        dispatch(getReviews());
+    }, []);
+
+    useEffect(() => {
+        setPaggArr(createNestedList(reviewsArr, 4));
+    }, [reviewsArr]);
+
+    useEffect(() => {
+        setReviewsState(paggArr[0]);
+    }, [paggArr]);
+
+    return (
+        <section className="reviewsBlock2">
+            <div className="container">
+                <div className="reviewsBlock2_btns">
+                    <button
+                        className="reviewsBlock2_btns_filter"
+                        onClick={() => {
+                            setShowFilterPopup(!showFilterPopup);
+                        }}
+                    >
+                        <img src={reviewsBlock2Filter} alt="" />
+                        Filter
+                    </button>
+                    <ReviewsFilterPopup showFilterPopup={showFilterPopup} />
+                    <div className="reviewsBlock2_btns_right">
+                        <button>All</button>
+                        <button>Review</button>
+                        <button>Video review</button>
+                    </div>
+                </div>
+                <h1 className="reviewsBlock2_title">Video review</h1>
+                <div className="row">
+                    {reviewsState?.map((item) => (
+                        <div className="col-3" key={item.id}>
+                            <div className="reviewsBlock2_card">
+                                <div style={{ position: "relative" }}>
+                                    <button className="reviewsBlock2_card_btn">
+                                        Play video
+                                    </button>
+                                    <img
+                                        src={reviewsBlock2Card}
+                                        alt=""
+                                        className="reviewsBlock2_card_img"
+                                    />
+                                </div>
+                                <h3 className="reviewsBlock2_card_title">
+                                    {item.name}
+                                </h3>
+                                <div className="reviewsBlock2_card_raiting">
+                                    <div className="reviewsBlock2_card_raiting_icons">
+                                        <StyledRating
+                                            name="read-only"
+                                            value={item.stars}
+                                            readOnly
+                                            sx={{
+                                                fontSize: "30px",
+                                            }}
+                                        />
+                                    </div>
+                                    <p className="reviewsBlock2_card_raiting_count">
+                                        {item.stars}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="reviewsBlock2_bottom_row">
+                    <div className="reviewsBlock2_bottom">
+                        <button
+                            className="reviewsBlock2_bottom_btn"
+                            onClick={() => prevPage()}
+                        >
+                            <FontAwesomeIcon icon={faChevronLeft} />
+                        </button>
+                        <div className="reviewsBlock2_bottom_center">
+                            {paggArr?.map((item, index) => (
+                                <p
+                                    key={index}
+                                    onClick={() => {
+                                        setPage(index);
+                                        setCurrentPage(index);
+                                    }}
+                                    className="reviewsBlock2_bottom_center_item"
+                                >
+                                    {index + 1}
+                                </p>
+                            ))}
+                            {/* <p className="reviewsBlock2_bottom_center_item">
+                                2
+                            </p>
+                            <p className="reviewsBlock2_bottom_center_item">
+                                3
+                            </p>
+                            <p className="reviewsBlock2_bottom_center_item">
+                                . . . . .
+                            </p>
+                            <p className="reviewsBlock2_bottom_center_item">
+                                16
+                            </p> */}
+                        </div>
+                        <button
+                            className="reviewsBlock2_bottom_btn"
+                            onClick={() => nextPage()}
+                        >
+                            <FontAwesomeIcon icon={faChevronRight} />
+                        </button>
+                    </div>
+                </div>
             </div>
-            <button className="reviewsBlock2_bottom_btn">
-              <FontAwesomeIcon icon={faChevronRight} />
-            </button>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default ReviewsBlock2;
--- front/src/components/Services1/ServicesBlock/ServBlock2/ServBlock2.jsx
+++ front/src/components/Services1/ServicesBlock/ServBlock2/ServBlock2.jsx
@@ -4,80 +4,92 @@ import i1 from "../../../../images/i1.png";
 import i2 from "../../../../images/i2.png";
 import i3 from "../../../../images/i3.png";
 import { useDispatch, useSelector } from "react-redux";
-import { createNestedList, getServicesSingle } from "../../../../redux/reducers/app";
+import {
+    createNestedList,
+    getServicesSingle,
+} from "../../../../redux/reducers/app";
 import { useParams } from "react-router-dom";
 import ServBlock2Card from "./ServBlock2Card";
 import { useNavigate } from "react-router-dom";
 
 const ServBlock2 = () => {
     const navigate = useNavigate();
-    const servicesSingle = useSelector(s => s.app.servicesSingle);
-    const currentLocation = useSelector(s => s.app.currentLocation);
-    const dispatch = useDispatch()
+    const servicesSingle = useSelector((s) => s.app.servicesSingle);
+    const currentLocation = useSelector((s) => s.app.currentLocation);
+    const dispatch = useDispatch();
     const params = useParams();
     const [childListWithPhoto, setChildListWithPhoto] = useState([]);
     const [childListWithoutPhoto, setChildListWithoutPhoto] = useState([]);
 
-    const setOfferList = () =>{
-        switch(true){
-            case servicesSingle.children.length < 3:{
-                setChildListWithPhoto(servicesSingle.children.map(item=>{
-                    return item
-                }))
+    const setOfferList = () => {
+        switch (true) {
+            case servicesSingle.children.length < 3: {
+                setChildListWithPhoto(
+                    servicesSingle.children.map((item) => {
+                        return item;
+                    })
+                );
             }
-            default:{
-                setChildListWithPhoto(servicesSingle.children.reduce((acc, rec,idx)=>{
-                    if(idx < 3){
-                        return [...acc, rec]
-                    } else{
-                        return acc
-                    }
-                },[]))
+            default: {
+                setChildListWithPhoto(
+                    servicesSingle.children.reduce((acc, rec, idx) => {
+                        if (idx < 3) {
+                            return [...acc, rec];
+                        } else {
+                            return acc;
+                        }
+                    }, [])
+                );
             }
         }
-    }
+    };
 
-    useEffect(()=>{
-       dispatch(getServicesSingle(params.slug, currentLocation.id));
+    useEffect(() => {
+        dispatch(getServicesSingle(params.slug, currentLocation.id));
     }, [params, currentLocation]);
-    useEffect(()=>{
-        if(JSON.stringify(servicesSingle) !== '{}'){
-           setOfferList()
-            const bigList = servicesSingle.children.filter((item, idx) =>{
-                return idx > 2
-            })
-            setChildListWithoutPhoto(createNestedList(bigList))
+    useEffect(() => {
+        if (JSON.stringify(servicesSingle) !== "{}") {
+            setOfferList();
+            const bigList = servicesSingle.children.filter((item, idx) => {
+                return idx > 2;
+            });
+            setChildListWithoutPhoto(createNestedList(bigList));
         }
-    }, [servicesSingle])
+    }, [servicesSingle]);
     return (
         <div className="servBlock2 container">
             <div className="servBlock2-box">
                 <div className="for-image-block">
-                {
-                    setChildListWithPhoto.length === 0
-                    ? ''
-                    :childListWithPhoto.map(item =>{
-                        return <ServBlock2Card key={item.id} item={item}/>
-                    })
-                }
-
+                    {setChildListWithPhoto.length === 0
+                        ? ""
+                        : childListWithPhoto.map((item) => {
+                              return (
+                                  <ServBlock2Card key={item.id} item={item} />
+                              );
+                          })}
                 </div>
                 {/* //////////////////////// */}
                 <div className="textaboveserv">
-                    {
-                        childListWithoutPhoto.map((item, idx) =>{
-                            return <ul key={idx}>
-                                    {
-                                        item.map(element =>{
-                                            return <li onClick={()=>{
-                                                navigate(`/services/${item.slug}`)
-                                              }}  key={element.id}>{element.name}</li>
-                                        })
-                                    }
+                    {childListWithoutPhoto.map((item, idx) => {
+                        return (
+                            <ul key={idx}>
+                                {item.map((element) => {
+                                    return (
+                                        <li
+                                            onClick={() => {
+                                                navigate(
+                                                    `/services/${item.slug}`
+                                                );
+                                            }}
+                                            key={element.id}
+                                        >
+                                            {element.name}
+                                        </li>
+                                    );
+                                })}
                             </ul>
-                        })
-                    }
-
+                        );
+                    })}
                 </div>
             </div>
         </div>
