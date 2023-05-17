2f406cbff72f2d057426d078a11cfdab510a849b
unknown <timurshermamatov1@gmail.com>
2023-05-18 02:49:56 +0600

    front

---
+++ front/src/Pages/BlogSinglePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BlogSingle from "../components/BlogSingle/BlogSingle";
+
+const BlogSinglePage = () => {
+    return (
+        <div>
+            <BlogSingle />
+        </div>
+    );
+};
+
+export default BlogSinglePage;
