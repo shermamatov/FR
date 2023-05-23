94881fe656815acbf7d1b5e1117076a4df4f0399
unknown <timurshermamatov1@gmail.com>
2023-05-23 13:12:21 +0600

    front

--- front-next/app/blog/page.tsx
+++ front-next/app/blog/page.tsx
@@ -1,23 +1,229 @@
-import { fetchBlog } from '@/api';
-import { Layout } from '@/components/Layout';
-import Link from 'next/link';
-import { use } from 'react';
-
+import { fetchBlog } from "@/api";
+import { Layout } from "@/components/Layout";
+import Image from "next/image";
+import Link from "next/link";
+import { use } from "react";
+import bath from "@/assets/bath.png";
 export default function Blog() {
-  const posts = use(fetchBlog());
+    const posts = use(fetchBlog());
 
-  console.log('posts', posts)
+    console.log("posts", posts);
 
-  return (
-    <Layout className="content">
-      <h1 className="mb-14">Blog</h1>
-      <div></div>
+    return (
+        <Layout className="content">
+            {/* <h1 className="mb-14">Blog</h1>
+            <div></div>
+            <div className="grid grid-cols-3 gap-x-4 gap-y-9">
+                {posts?.results.map((item) => (
+                    <Link key={item.id} href={`/blog/${item.id}`}>
+                        {item.title}
+                    </Link>
+                ))}
+            </div> */}
+            <main className="blog">
+                <div className="blog-box">
+                    <section className="all-title">
+                        <ul className="all-title-block">
+                            <li className="title-li">All</li>
+                            <li
+                                style={{ color: "#9B51E0" }}
+                                className="title-li"
+                            >
+                                Cooling
+                            </li>
+                            <li
+                                style={{ color: "#107EBD" }}
+                                className="title-li"
+                            >
+                                Business Strategy{" "}
+                            </li>
+                            <li
+                                style={{ color: "#EC2A48" }}
+                                className="title-li"
+                            >
+                                Heating{" "}
+                            </li>
+                            <li
+                                style={{ color: "#F2994A" }}
+                                className="title-li"
+                            >
+                                Repairing
+                            </li>
+                            <li
+                                style={{ color: "#219653" }}
+                                className="title-li"
+                            >
+                                Commercial
+                            </li>
+                        </ul>
+                    </section>
+                    <section className="about-infor">
+                        <div className="about-infor-box">
+                            <div className="description-image">
+                                {/* <img src={person} alt="" /> */}
+                            </div>
+                            <div className="title-for">
+                                <div className="dsa">
+                                    <span
+                                        style={{
+                                            color: "#107EBD",
+                                            fontWeight: "700",
+                                        }}
+                                    >
+                                        Business Strategy{" "}
+                                    </span>
+                                </div>
+                                <div>
+                                    <span
+                                        style={{
+                                            color: "#72421C",
+                                            fontWeight: "600",
+                                            lineHeight: "15px",
+                                        }}
+                                    >
+                                        December 30, 2019
+                                    </span>
+                                </div>
+                                <h1 className="h1-a-server">
+                                    A-Series Ultra Low NOx Gas Furnace 80%
+                                    Single Stage 40,000 BTU Non-Variable
+                                    Upflow/Horizontal 14.5 wide
+                                </h1>
+                                <p className="a-global">
+                                    A global print fulfillment company partnered
+                                    with Mason Interactive to grow omnichannel
+                                    sales via Search Engine Optimization (SEO).
+                                    During the course of the engagement, the
+                                    client saw a 134% increase in organic
+                                    traffic, resulting in a measurable increase
+                                    in leads, and sales. the client saw a 134%
+                                    increase in organic traffic, resulting in a
+                                    measurable increase in leads, and sales.
+                                    <div>
+                                        <span style={{ color: "#F2994A" }}>
+                                            Read More article
+                                        </span>
+                                    </div>
+                                </p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="two-fail">
+                        <div className="two-fail-box">
+                            <div className="two">
+                                {/* <img src={person2} alt="" /> */}
+                                <div>
+                                    <span
+                                        style={{
+                                            color: "#F2994A",
+                                            lineHeight: "2",
+                                        }}
+                                    >
+                                        Heating
+                                    </span>
+                                </div>
+                                <h3>How, when and why we stay</h3>
+                                <p>
+                                    Staying in touch with customers is important
+                                    for any company; it often forms part of any
+                                    good marketing strategy.
+                                </p>
+                                <span style={{ color: "#F2994A" }}>
+                                    Read More article
+                                </span>
+                            </div>
 
-      <div className="grid grid-cols-3 gap-x-4 gap-y-9">
-        {posts?.results.map((item) => (
-          <Link key={item.id} href={`/blog/${item.id}`}>{item.title}</Link>
-        ))}
-      </div>
-    </Layout>
-  );
+                            <div className="two">
+                                {/* <img src={person3} alt="" /> */}
+                                <div>
+                                    <span
+                                        style={{
+                                            color: "#F2994A",
+                                            lineHeight: "2",
+                                        }}
+                                    >
+                                        Heating
+                                    </span>
+                                </div>
+                                <h3>How, when and why we stay</h3>
+                                <p>
+                                    Staying in touch with customers is important
+                                    for any company; it often forms part of any
+                                    good marketing strategy.{" "}
+                                </p>
+                                <span style={{ color: "#F2994A" }}>
+                                    Read More article
+                                </span>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="four-faiis">
+                        <div className="two-fail-boxs">
+                            {posts?.results.map((item) => {
+                                return (
+                                    <div
+                                        key={item.id}
+                                        className="two-fail-boxs-cols"
+                                    >
+                                        <div className="twos">
+                                            <Image
+                                                src={bath}
+                                                alt=""
+                                                width={100}
+                                                height={100}
+                                            />
+                                            <div>
+                                                <span
+                                                    style={{
+                                                        color: "#F2994A",
+                                                        lineHeight: "2",
+                                                    }}
+                                                >
+                                                    Heating
+                                                </span>
+                                            </div>
+                                            <div>
+                                                <span
+                                                    style={{
+                                                        color: "#72421C",
+                                                        lineHeight: "2",
+                                                        fontWeight: "600",
+                                                    }}
+                                                >
+                                                    {item.created_at.slice(
+                                                        0,
+                                                        10
+                                                    )}
+                                                </span>
+                                            </div>
+                                            <h3>
+                                                {item.title.length > 21
+                                                    ? item.title
+                                                          .slice(0, 18)
+                                                          .trim() + "..."
+                                                    : item.title}
+                                            </h3>
+                                            <p>
+                                                {item.description.length > 100
+                                                    ? item.description
+                                                          .slice(0, 97)
+                                                          .trim() + "..."
+                                                    : item.description}
+                                            </p>
+                                            <Link
+                                                key={item.id}
+                                                href={`/blog/${item.id}`}
+                                            >
+                                                <span>Read More article</span>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </section>
+                </div>
+            </main>
+        </Layout>
+    );
 }
