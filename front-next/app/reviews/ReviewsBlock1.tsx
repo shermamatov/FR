bcdd7907dbeff52af1491d22542f0f3a60f09b9d
unknown <timurshermamatov1@gmail.com>
2023-05-31 23:35:28 +0600

    fix review adaptive

--- front-next/app/reviews/ReviewsBlock1.tsx
+++ front-next/app/reviews/ReviewsBlock1.tsx
@@ -4,10 +4,11 @@ import starIcon from "./img/star-icon.svg";
 import circleImg from "./img/circle-img.svg";
 import bigImg from "./img/bg.jpg";
 import ReviewsWritePopup from "./ReviewsPopup/ReviewsWritePopup/ReviewsWritePopup";
+import { PaginationData, Service } from "@/api";
 
-export default function ReviewsBlock1({ services }) {
+export default function ReviewsBlock1({ services }: any) {
     function showReviewsWritePopup() {
-        let popup = document.getElementById("reviewsWritePopup");
+        let popup: any = document.getElementById("reviewsWritePopup");
         popup.className += " reviewsWritePopup-show";
     }
 
@@ -15,17 +16,22 @@ export default function ReviewsBlock1({ services }) {
         <div className="reviewsBlock1">
             <ReviewsWritePopup services={services} />
             <div className="content">
-                <div className="grid grid-cols-2">
+                <div className="grid grid-cols-1 md:grid-cols-2">
                     <div>
                         <h1 className="reviewsBlock1_subtitle">Review</h1>
-                        <h2>Overall Rating from 129 Reviews</h2>
+                        <h2
+                            className="text-3xl sm:text-5xl
+                        "
+                        >
+                            Overall Rating from 129 Reviews
+                        </h2>
                         <div className="reviewsBlock1_stars">
                             <div className="reviewsBlock1_stars_icon">
-                                <Image src={starIcon} alt="" />
-                                <Image src={starIcon} alt="" />
-                                <Image src={starIcon} alt="" />
-                                <Image src={starIcon} alt="" />
-                                <Image src={starIcon} alt="" />
+                                <Image className="w-7" src={starIcon} alt="" />
+                                <Image className="w-7" src={starIcon} alt="" />
+                                <Image className="w-7" src={starIcon} alt="" />
+                                <Image className="w-7" src={starIcon} alt="" />
+                                <Image className="w-7" src={starIcon} alt="" />
                             </div>
                             <h4 className="reviewsBlock1_stars_count">
                                 5.0/4.7
@@ -95,7 +101,7 @@ export default function ReviewsBlock1({ services }) {
                         </button>
                     </div>
 
-                    <div className="reviewsBlock1_images">
+                    <div className="reviewsBlock1_images hidden md:block">
                         <Image
                             src={circleImg}
                             className="reviewsBlock1_circleImg"
