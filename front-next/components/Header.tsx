d9e5a3df71fa4285856d61547f9d66a7f259c768
Пользователь <dastan@yordstudio.com>
2023-06-02 01:03:00 +0600

    test push Dastan

--- front-next/components/Header.tsx
+++ front-next/components/Header.tsx
@@ -39,7 +39,7 @@ const Header = () => {
   // useEffect(() => {
   //     getLocat();
   // }, [localStorage.getItem("currentLocation")]);
-  console.log(services);
+  // console.log(services);
   return (
     <div>
       <header>
