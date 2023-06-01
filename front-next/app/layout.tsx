0915b46d43bbcef444b221bba1f0d3f9af26cb95
unknown <timurshermamatov1@gmail.com>
2023-06-01 19:13:56 +0600

    footer

--- front-next/app/layout.tsx
+++ front-next/app/layout.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import "./globals.css";
 import { Alfa_Slab_One, Montserrat } from "next/font/google";
+import Footer from "@/components/Footer";
 
 export const fontAlfa = Alfa_Slab_One({
     weight: ["400"],
@@ -33,62 +34,7 @@ export default function RootLayout({
         <html lang="en" className={clsx(fontMain.variable, fontAlfa.variable)}>
             <body>
                 {children}
-                <footer>
-                    <div className="content grid grid-cols-3 gap-5 pt-14 pb-40">
-                        <section>
-                            <h3>About</h3>
-                            <p>
-                                We are an award winning HVAC company with over
-                                10+ years experience in the business. We provide
-                                a wide range of services for both residential
-                                and commercial clients.
-                            </p>
-                            <a href="">facebook</a>
-                            <a href="">gmail</a>
-                            <a href="">yumi</a>
-                        </section>
-                        <section>
-                            <h3>Contacts</h3>
-                            <div>
-                                {/* icon */}
-                                <p>Address</p>
-                                <address className="font-bold">
-                                    73 Canal Street, New York, Los
-                                </address>
-                            </div>
-                            <div>
-                                {/* icon */}
-                                <p>Phone number</p>
-                                <a
-                                    href="tel:+18188881010"
-                                    className="font-bold"
-                                >
-                                    +1-818-888-10-10
-                                </a>
-                            </div>
-                            <div>
-                                {/* icon */}
-                                <p>E-mail</p>
-                                <a
-                                    href="tel:+18188881010"
-                                    className="font-bold"
-                                >
-                                    +1-818-888-10-10
-                                </a>
-                            </div>
-                        </section>
-                        <section>
-                            <h3>Quick links</h3>
-                            <nav className="grid divide-y-0 divide-white font-bold">
-                                <a href="/about-us">About us</a>
-                                <a href="/services">Services</a>
-                                <a href="/blog">Blog</a>
-                                <a href="/location">Locations</a>
-                                <a href="/services">Contacts</a>
-                            </nav>
-                        </section>
-                    </div>
-                </footer>
+                <Footer />
             </body>
         </html>
     );
