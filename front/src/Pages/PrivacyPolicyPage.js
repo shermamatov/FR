85ae635e8c14e0cc73dafdf20f116d8abf2674cb
amg <amg@iageengineering.net>
2023-06-04 22:51:45 +0300

    init new front

--- front/src/Pages/PrivacyPolicyPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import PrivacyPolicy from '../components/PrivacyPolicy/PrivacyPolicy'
-export default function PrivacyPolicyPage() {
-  return (
-    <div>
-        <PrivacyPolicy/>
-    </div>
-  )
-}
