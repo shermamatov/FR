15cff2bad1cf93b1592727cb9b7df6ccf00e31ad
Пользователь <dastan@yordstudio.com>
2023-06-05 20:49:57 +0600

    Image next fix

--- front/components/bookNow/booknowFirst.tsx
+++ front/components/bookNow/booknowFirst.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
 import "./booknowFirst.css";
-import blockNow1Circle from "@/assets/blockNow1-circle.png";
-import blockNow1Big from "@/assets/blockNow1-big.webp";
 export default function BooknowFirst() {
   return (
     <section className="blockNow1">
@@ -11,13 +8,13 @@ export default function BooknowFirst() {
         </h1>
         <div className="blockNow1_imgBlock">
           <div className="blockNow1_imgBlock_wraper">
-            <Image
-              src={blockNow1Circle}
+            <img
+              src="/blockNow1-circle.png"
               alt=""
               className="blockNow1_imgBlock_circle"
             />
-            <Image
-              src={blockNow1Big}
+            <img
+              src="/blockNow1-big.webp"
               alt=""
               className="blockNow1_imgBlock_big"
             />
