85c206b9518cd671070c7466ec9076d661158b7e
unknown <timurshermamatov1@gmail.com>
2023-06-05 15:07:28 +0600

    front

--- front/app/book/page.tsx
+++ front/app/book/page.tsx
@@ -1,16 +1,15 @@
 import { Layout } from "@/components/Layout";
 import BooknowFirst from "./bookpopup/booknow/booknowFirst";
 import BooknowSecond from "./bookpopup/booknowSecond/booknowSecond";
-import {fetchServices} from '@/api'
-import {use} from 'react';
+import { fetchServices } from "@/api";
+import { use } from "react";
 
 export default function Book() {
-  const services = use(fetchServices());
-  return (
-    <Layout className="">
-      {" "}
-      <BooknowFirst />
-      <BooknowSecond services={services.results} />
-    </Layout>
-  );
+    const services = use(fetchServices());
+    return (
+        <Layout className="">
+            <BooknowFirst />
+            <BooknowSecond services={services.results} />
+        </Layout>
+    );
 }
