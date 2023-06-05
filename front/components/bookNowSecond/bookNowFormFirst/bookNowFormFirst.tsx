be37bc2d1454722d0e4bcb1eb64432ef02ac6406
Пользователь <dastan@yordstudio.com>
2023-06-05 18:46:32 +0600

    localStorage error in book v2

--- front/components/bookNowSecond/bookNowFormFirst/bookNowFormFirst.tsx
+++ front/components/bookNowSecond/bookNowFormFirst/bookNowFormFirst.tsx
@@ -16,26 +16,16 @@ export default function BookNowFormFirst({
   const [email, setEamil] = useState("");
   const [address, setAddress] = useState("");
   const [service, setService] = useState<any>(null);
-  // const [location, setLocation] = useState(
-  //   localStorage.getItem("currentLocation") || ""
-  //     ? JSON.parse(localStorage.getItem("currentLocation") || "")
-  //     : {}
-  // );
-  const [location, setLocation] = useState();
-  const [currentLocation, setCurrentLocation] = useState({});
-  useEffect(() => {
-    if (
-      typeof window !== "undefined" &&
+  const [location, setLocation] = useState(
+    (typeof window !== "undefined" &&
       window.localStorage &&
-      localStorage.getItem("currentLocation")
-    ) {
-      setLocation(
-        localStorage.getItem("currentLocation") || ""
-          ? JSON.parse(localStorage.getItem("currentLocation") || "")
-          : {}
-      );
-    }
-  }, []);
+      localStorage.getItem("currentLocation")) ||
+      ""
+      ? JSON.parse(localStorage.getItem("currentLocation") || "")
+      : {}
+  );
+  const [currentLocation, setCurrentLocation] = useState({});
+
   useEffect(() => {
     if (JSON.stringify(location) === "{}") {
       axios("https://itek-dev.highcat.org/api/location/find/").then(
