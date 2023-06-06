136ace3f6ff32fd7574e312e9df224d16d4e39ab
unknown <timurshermamatov1@gmail.com>
2023-06-07 02:15:10 +0600

    front

--- front/components/bookNow/booknowFirst.tsx
+++ front/components/bookNow/booknowFirst.tsx
@@ -1,26 +1,26 @@
 import "./booknowFirst.css";
 export default function BooknowFirst() {
-  return (
-    <section className="blockNow1">
-      <div className="container">
-        <h1 className="blockNow1_text">
-          our team is always <br /> happy to help you
-        </h1>
-        <div className="blockNow1_imgBlock">
-          <div className="blockNow1_imgBlock_wraper">
-            <img
-              src="/blockNow1-circle.png"
-              alt=""
-              className="blockNow1_imgBlock_circle"
-            />
-            <img
-              src="/blockNow1-big.webp"
-              alt=""
-              className="blockNow1_imgBlock_big"
-            />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className="blockNow1">
+            <div className="content">
+                <h1 className="blockNow1_text">
+                    our team is always <br /> happy to help you
+                </h1>
+                <div className="blockNow1_imgBlock">
+                    <div className="blockNow1_imgBlock_wraper">
+                        <img
+                            src="/blockNow1-circle.png"
+                            alt=""
+                            className="blockNow1_imgBlock_circle"
+                        />
+                        <img
+                            src="/blockNow1-big.webp"
+                            alt=""
+                            className="blockNow1_imgBlock_big"
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
