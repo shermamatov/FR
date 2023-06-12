159d25dfc24b995edb78eb319ebf5d013c6a5452
DASTAN DOKTURBEKOV <dastanhayama@gmail.com>
2023-06-12 22:18:24 +0600

    notifications

--- front/app/blog/page.tsx
+++ front/app/blog/page.tsx
@@ -5,203 +5,188 @@ import { use, useMemo } from "react";
 import "./blogPage.scss";
 
 export default function Blog() {
-    const posts = use(fetchBlog());
+  const posts = use(fetchBlog());
 
-    // function getMiniData() {
-    //     const arr = [...posts.results];
-    //     return arr.slice(2);
-    // }
+  // function getMiniData() {
+  //     const arr = [...posts.results];
+  //     return arr.slice(2);
+  // }
 
-    const formatter = useMemo(
-        () =>
-            new Intl.DateTimeFormat("en-US", {
-                day: "numeric",
-                month: "short",
-                year: "numeric",
-            }),
-        []
-    );
+  const formatter = useMemo(
+    () =>
+      new Intl.DateTimeFormat("en-US", {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+      }),
+    []
+  );
 
-    return (
-        <Layout className="content">
-            <div className={"flex mb-8"}>
-                <p className="font-bold">All</p>
-                <p className="ml-5 font-bold" style={{ color: "#9B51E0" }}>
-                    Cooling
-                </p>
-                <p
-                    className="ml-5 font-bold"
-                    style={{ color: "#107EBD", whiteSpace: "nowrap" }}
-                >
-                    Business Strategy{" "}
-                </p>
-                <p className="ml-5 font-bold" style={{ color: "#EC2A48" }}>
-                    Heating{" "}
-                </p>
-                <p style={{ color: "#F2994A" }} className="ml-5 font-bold">
-                    Repairing
-                </p>
-                <p style={{ color: "#219653" }} className="ml-5 font-bold">
-                    Commercial
-                </p>
+  return (
+    <Layout className="content">
+      <div className={"flex mb-8"}>
+        <p className="font-bold">All</p>
+        <p className="ml-5 font-bold" style={{ color: "#9B51E0" }}>
+          Cooling
+        </p>
+        <p
+          className="ml-5 font-bold"
+          style={{ color: "#107EBD", whiteSpace: "nowrap" }}>
+          Business Strategy{" "}
+        </p>
+        <p className="ml-5 font-bold" style={{ color: "#EC2A48" }}>
+          Heating{" "}
+        </p>
+        <p style={{ color: "#F2994A" }} className="ml-5 font-bold">
+          Repairing
+        </p>
+        <p style={{ color: "#219653" }} className="ml-5 font-bold">
+          Commercial
+        </p>
+      </div>
+      <div className="blog_block1">
+        <div className="blog_block1_left">
+          <img src="/bath.png" alt="" />
+        </div>
+        <div className="blog_block1_right">
+          <div style={{ color: "#107EBD", whiteSpace: "nowrap" }}>
+            Business Strategy
+          </div>
+          <div>
+            <span
+              style={{
+                color: "#72421C",
+                fontWeight: "600",
+                lineHeight: "15px",
+              }}>
+              December 30, 2019
+            </span>
+          </div>
+          <h1>
+            A-Series Ultra Low NOx Gas Furnace 80% Single Stage 40,000 BTU
+            Non-Variable Upflow/ Horizontal 14.5 wide
+          </h1>
+          <p>
+            A global print fulfillment company partnered with Mason Interactive
+            to grow omnichannel sales via Search Engine Optimization (SEO).
+            During the course of the engagement, the client saw a 134% increase
+            in organic traffic, resulting in a measurable increase in leads, and
+            sales. the client saw a 134% increase in organic traffic, resulting
+            in a measurable increase in leads, and sales.
+          </p>
+          <a href="#" style={{ color: "#F2994A" }}>
+            Read More article
+          </a>
+        </div>
+      </div>
+      <div className="blog_block2">
+        {posts.results.slice(0, 2).map((item) => (
+          <div
+            className="blog_card"
+            style={{ position: "relative" }}
+            key={item.id}>
+            <img src={item.image1 || "/bath.png"} alt="" />
+            <div
+              style={{
+                color: "#F2994A",
+                fontWeight: "700",
+                marginBottom: "4px",
+              }}>
+              Heating
             </div>
-            <div className="blog_block1">
-                <div className="blog_block1_left">
-                    <img src="/bath.png" alt="" />
-                </div>
-                <div className="blog_block1_right">
-                    <div style={{ color: "#107EBD", whiteSpace: "nowrap" }}>
-                        Business Strategy
-                    </div>
-                    <div>
-                        <span
-                            style={{
-                                color: "#72421C",
-                                fontWeight: "600",
-                                lineHeight: "15px",
-                            }}
-                        >
-                            December 30, 2019
-                        </span>
-                    </div>
-                    <h1>
-                        A-Series Ultra Low NOx Gas Furnace 80% Single Stage
-                        40,000 BTU Non-Variable Upflow/ Horizontal 14.5 wide
-                    </h1>
-                    <p>
-                        A global print fulfillment company partnered with Mason
-                        Interactive to grow omnichannel sales via Search Engine
-                        Optimization (SEO). During the course of the engagement,
-                        the client saw a 134% increase in organic traffic,
-                        resulting in a measurable increase in leads, and sales.
-                        the client saw a 134% increase in organic traffic,
-                        resulting in a measurable increase in leads, and sales.
-                    </p>
-                    <a href="#" style={{ color: "#F2994A" }}>
-                        Read More article
-                    </a>
-                </div>
-            </div>
-            <div className="blog_block2">
-                {posts.results.slice(0, 2).map((item) => (
-                    <div
-                        className="blog_card"
-                        style={{ position: "relative" }}
-                        key={item.id}
-                    >
-                        <img src={item.image1 || "/bath.png"} alt="" />
-                        <div style={{ color: "#F2994A", fontWeight: "700" }}>
-                            Heating
-                        </div>
-                        <span
-                            style={{
-                                color: "#72421C",
-                                fontWeight: "600",
-                                lineHeight: "15px",
-                            }}
-                        >
-                            {formatter.format(new Date(item.created_at))}
-                        </span>
-                        <h3>{item.title}</h3>
-                        <p>{item.description}</p>
-                        <Link href={`/blog/${item.id}`}>
-                            <span
-                                style={{
-                                    color: "#F2994A",
-                                    position: "absolute",
-                                    bottom: "10px",
-                                    left: "10px",
-                                }}
-                            >
-                                Read More article
-                            </span>
-                        </Link>
-                    </div>
-                ))}
-            </div>
+            <span
+              style={{
+                color: "#72421C",
+                fontWeight: "600",
+                lineHeight: "15px",
+              }}>
+              {formatter.format(new Date(item.created_at))}
+            </span>
+            <h3 className="blog-card-title">{item.title}</h3>
+            <p className="blog-card-desc">{item.description}</p>
+            <Link href={`/blog/${item.id}`}>
+              <span
+                style={{
+                  color: "#F2994A",
+                  position: "absolute",
+                  bottom: "10px",
+                  left: "10px",
+                }}>
+                Read More article
+              </span>
+            </Link>
+          </div>
+        ))}
+      </div>
 
-            <div className="blog_block3 hidden md:grid">
-                {posts.results.slice(4).map((item) => (
-                    <div className="blog_card" key={item.id}>
-                        <img
-                            src={item.image1 ? item.image1 : "/bath.png"}
-                            alt=""
-                        />
-                        <div style={{ color: "#F2994A", fontWeight: "700" }}>
-                            Heating
-                        </div>
-                        <span
-                            style={{
-                                color: "#72421C",
-                                fontWeight: "600",
-                                lineHeight: "15px",
-                                fontSize: "12px",
-                            }}
-                        >
-                            {formatter.format(new Date(item.created_at))}
-                        </span>
-                        <h3>{item.title}</h3>
-                        <p>
-                            {item.description.length > 200
-                                ? item.description.slice(0, 200) + " ..."
-                                : item.description}
-                        </p>
-                        <Link href={`/blog/${item.id}`}>
-                            <span
-                                style={{
-                                    color: "#F2994A",
-                                    position: "absolute",
-                                    bottom: "10px",
-                                    left: "10px",
-                                }}
-                            >
-                                Read More article
-                            </span>
-                        </Link>
-                    </div>
-                ))}
-            </div>
-            <div className="blog_block3 grid md:hidden">
-                {posts.results.map((item) => (
-                    <div className="blog_card" key={item.id}>
-                        <img
-                            src={item.image1 ? item.image1 : "/bath.png"}
-                            alt=""
-                        />
-                        <div style={{ color: "#F2994A", fontWeight: "700" }}>
-                            Heating
-                        </div>
-                        <span
-                            style={{
-                                color: "#72421C",
-                                fontWeight: "600",
-                                lineHeight: "15px",
-                                fontSize: "12px",
-                            }}
-                        >
-                            December 30, 2019
-                        </span>
-                        <h3>{item.title}</h3>
-                        <p>
-                            {item.description.length > 200
-                                ? item.description.slice(0, 200) + " ..."
-                                : item.description}
-                        </p>
-                        <Link href={`/blog/${item.id}`}>
-                            <span
-                                style={{
-                                    color: "#F2994A",
-                                    position: "absolute",
-                                    bottom: "10px",
-                                    left: "10px",
-                                }}
-                            >
-                                Read More article
-                            </span>
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        </Layout>
-    );
+      <div className="blog_block3 hidden md:grid">
+        {posts.results.slice(4).map((item) => (
+          <div className="blog_card" key={item.id}>
+            <img src={item.image1 ? item.image1 : "/bath.png"} alt="" />
+            <div style={{ color: "#F2994A", fontWeight: "700" }}>Heating</div>
+            <span
+              style={{
+                color: "#72421C",
+                fontWeight: "600",
+                lineHeight: "15px",
+                fontSize: "12px",
+              }}>
+              {formatter.format(new Date(item.created_at))}
+            </span>
+            <h3>{item.title}</h3>
+            <p>
+              {item.description.length > 200
+                ? item.description.slice(0, 200) + " ..."
+                : item.description}
+            </p>
+            <Link href={`/blog/${item.id}`}>
+              <span
+                style={{
+                  color: "#F2994A",
+                  position: "absolute",
+                  bottom: "10px",
+                  left: "10px",
+                }}>
+                Read More article
+              </span>
+            </Link>
+          </div>
+        ))}
+      </div>
+      <div className="blog_block3 grid md:hidden">
+        {posts.results.map((item) => (
+          <div className="blog_card" key={item.id}>
+            <img src={item.image1 ? item.image1 : "/bath.png"} alt="" />
+            <div style={{ color: "#F2994A", fontWeight: "700" }}>Heating</div>
+            <span
+              style={{
+                color: "#72421C",
+                fontWeight: "600",
+                lineHeight: "15px",
+                fontSize: "12px",
+              }}>
+              December 30, 2019
+            </span>
+            <h3>{item.title}</h3>
+            <p>
+              {item.description.length > 200
+                ? item.description.slice(0, 200) + " ..."
+                : item.description}
+            </p>
+            <Link href={`/blog/${item.id}`}>
+              <span
+                style={{
+                  color: "#F2994A",
+                  position: "absolute",
+                  bottom: "10px",
+                  left: "10px",
+                }}>
+                Read More article
+              </span>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 }
