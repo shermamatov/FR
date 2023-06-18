bdddca058838ce22e8a1d9d2e3bdd8253955a235
DASTAN DOKTURBEKOV <dastanhayama@gmail.com>
2023-06-18 21:22:47 +0600

    reviews ready

--- front/app/reviews/ReviewsPopup/ReviewsWritePopup/ReviewsWritePopup.tsx
+++ front/app/reviews/ReviewsPopup/ReviewsWritePopup/ReviewsWritePopup.tsx
@@ -12,194 +12,229 @@ import "react-toastify/dist/ReactToastify.css";
 import { Rating } from "@mui/material";
 
 const ReviewsWritePopup = ({ services }: any) => {
-    const [formSelector, setFormSelector] = useState("write");
-    const [stars, setStars] = useState<any>(0);
-    const [name, setName] = useState("");
-    const [review_text, setReview_text] = useState("");
-    const [service, setService] = useState<any>(null);
-    const [loading, setLoading] = useState(false);
-    const [message, setMessage] = useState("");
-    const [video, setVideo] = useState<any>({});
-    const formData: any = new FormData();
-    console.log(video);
+  const [formSelector, setFormSelector] = useState("write");
+  const [stars, setStars] = useState<any>(0);
+  const [name, setName] = useState("");
+  const [review_text, setReview_text] = useState("");
+  const [service, setService] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [video, setVideo] = useState<any>({});
+  const formData: any = new FormData();
+  const [location, setLocation] = useState<any>();
+  const [locationId, setLocationId] = useState(
+    (typeof window !== "undefined" &&
+      window.localStorage &&
+      localStorage.getItem("currentLocation")) ||
+      ""
+      ? JSON.parse(localStorage.getItem("currentLocation") || "")
+      : {}
+  );
 
-    const notifyError = () => {
-        toast.error("An error has occurred. Please try again.", {
-            position: toast.POSITION.BOTTOM_RIGHT,
-            autoClose: 2000,
-            className: "custom-toast",
-        });
-    };
-    const notifySuccess = () => {
-        toast.success("Thank you for your review!", {
-            position: toast.POSITION.BOTTOM_RIGHT,
-            autoClose: 2000,
-            className: "custom-toast-success",
-        });
-    };
-
-    const submitReview = () => {
-        switch (formSelector) {
-            case "write": {
-                if (
-                    name.trim() &&
-                    review_text.trim() &&
-                    stars &&
-                    service &&
-                    location
-                ) {
-                    setLoading(true);
-                    formData.append("name", name);
-                    formData.append("stars", stars);
-                    formData.append("review_text", review_text);
-                    formData.append("service", service);
-                    formData.append("location", location);
-                    formData.append("video", null);
-                    formData.append("image1", null);
-                    formData.append("image2", null);
-                    formData.append("phone", "");
-                    formData.append("email", "");
-                    axios
-                        .post(
-                            "https://1furniturerestoration.com/api/review",
-                            formData
-                        )
-                        .then((response) => {
-                            console.log(response);
-                            notifySuccess();
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                            notifyError();
-                        })
-                        .finally(() => {
-                            setLoading(false);
-                        });
-                }
-                break;
-            }
-            case "video": {
-                if (name.trim() && video.name && stars && service && location) {
-                    setLoading(true);
-                    formData.append("name", name);
-                    formData.append("stars", stars);
-                    formData.append("review_text", "");
-                    formData.append("service", service);
-                    formData.append("location", location);
-                    formData.append("video", video, video.name);
-                    formData.append("image1", null);
-                    formData.append("image2", null);
-                    formData.append("phone", "");
-                    formData.append("email", "");
-                    axios
-                        .post(
-                            "https://1furniturerestoration.com/api/review",
-                            formData
-                        )
-                        .then((response) => {
-                            console.log(response);
-                            notifySuccess();
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                            notifyError();
-                        })
-                        .finally(() => {
-                            setLoading(false);
-                        });
-                }
-                break;
-            }
-        }
-    };
-
-    function closeReviewsWritePopup() {
-        let popup: any = document.getElementById("reviewsWritePopup");
-        popup.className = "reviewsWritePopup";
+  useEffect(() => {
+    if (JSON.stringify(location) === "{}") {
+      axios("https://itek-dev.highcat.org/api/location/find/").then(
+        ({ data }) => setLocation(data)
+      );
     }
+  }, []);
+  const notifyError = () => {
+    toast.error("An error has occurred. Please try again.", {
+      position: toast.POSITION.BOTTOM_RIGHT,
+      autoClose: 2000,
+      className: "custom-toast",
+    });
+  };
+  const notifySuccess = () => {
+    toast.success("Thank you for your review!", {
+      position: toast.POSITION.BOTTOM_RIGHT,
+      autoClose: 2000,
+      className: "custom-toast-success",
+    });
+  };
+  useEffect(() => {
+    if (locationId) {
+      setLocation(locationId.id);
+    }
+  }, [location]);
+  useEffect(() => {
+    console.log(
+      "stars",
+      stars,
+      "name",
+      name,
+      "review",
+      review_text,
+      "service",
+      service,
+      "video",
+      video,
+      "location",
+      location
+    );
+  }, [stars, name, review_text, service, video, location]);
 
-    return (
-        <div className={"reviewsWritePopup"} id="reviewsWritePopup">
-            <ToastContainer />
-            <div
-                onClick={() => closeReviewsWritePopup()}
-                className="reviewsWritePopup_left"
-            ></div>
-            <div className="reviewsWritePopup_right">
-                <div className="reviewsWritePopup_header">
-                    <button
-                        onClick={() => closeReviewsWritePopup()}
-                        className="reviewsWritePopup_logo"
-                    >
-                        <img src="/logo.svg" alt="" />
-                    </button>
-                    <h1 className="reviewsWritePopup_title">Write a review</h1>
+  const submitReview = () => {
+    switch (formSelector) {
+      case "write": {
+        if (name.trim() && review_text.trim() && stars && service && location) {
+          setLoading(true);
+          formData.append("name", name);
+          formData.append("stars", stars);
+          formData.append("review_text", review_text);
+          formData.append("service", service);
+          formData.append("location", location);
+          formData.append("video", "");
+          formData.append("image1", "");
+          formData.append("image2", "");
+          formData.append("phone", "");
+          formData.append("email", "");
 
-                    <div className="reviewsWritePopup_header_right">
-                        <button
-                            onClick={() => setFormSelector("write")}
-                            className={
-                                formSelector === "write"
-                                    ? "reviewsWritePopup_header_btn reviewsWritePopup_header_btn-active"
-                                    : "reviewsWritePopup_header_btn"
-                            }
-                        >
-                            review
-                        </button>
-                        <button
-                            onClick={() => setFormSelector("video")}
-                            className={
-                                formSelector === "video"
-                                    ? "reviewsWritePopup_header_btn reviewsWritePopup_header_btn-active"
-                                    : "reviewsWritePopup_header_btn"
-                            }
-                        >
-                            Video review
-                        </button>
-                    </div>
+          axios({
+            method: "post",
+            url: "https://1furniturerestoration.com/api/review/",
+            data: formData,
+            headers: { "Content-Type": "multipart/form-data" },
+          })
+            //   axios
+            //     .post(
+            //       "https://1furniturerestoration.com/api/review/",
+            //       JSON.stringify(formData),
+            //       axiosConfig
+            //     )
+            .then((response) => {
+              console.log(response);
+              notifySuccess();
+            })
+            .catch((error) => {
+              console.log(error);
+              notifyError();
+            })
+            .finally(() => {
+              setLoading(false);
+            });
+        }
+        break;
+      }
+      case "video": {
+        if (name.trim() && video.name && stars && service && location) {
+          setLoading(true);
+          formData.append("name", name);
+          formData.append("stars", stars);
+          formData.append("review_text", "Video Review");
+          formData.append("service", service);
+          formData.append("location", location);
+          formData.append("video", video);
+          formData.append("image1", "");
+          formData.append("image2", "");
+          formData.append("phone", "");
+          formData.append("email", "");
+          axios({
+            method: "post",
+            url: "https://1furniturerestoration.com/api/review/",
+            data: formData,
+            headers: { "Content-Type": "multipart/form-data" },
+          })
+            //   axios
+            //     .post("https://1furniturerestoration.com/api/review", formData)
+            .then((response) => {
+              console.log(response);
+              notifySuccess();
+            })
+            .catch((error) => {
+              console.log(error);
+              notifyError();
+            })
+            .finally(() => {
+              setLoading(false);
+            });
+        }
+        break;
+      }
+    }
+  };
 
-                    <button
-                        onClick={() => closeReviewsWritePopup()}
-                        className="reviewsWritePopup_header_closeBtn"
-                    >
-                        X
-                    </button>
-                </div>
+  function closeReviewsWritePopup() {
+    let popup: any = document.getElementById("reviewsWritePopup");
+    popup.className = "reviewsWritePopup";
+  }
 
-                <div className="reviewsWritePopup_name">
-                    <h2 className="reviewsWritePopup_name_title">Full Name</h2>
-                    <input
-                        type="text"
-                        placeholder="Enter your full name"
-                        className="reviewsWritePopup_name_input"
-                        onChange={(e) => setName(e.target.value)}
-                        value={name}
-                    />
-                </div>
+  return (
+    <div className={"reviewsWritePopup"} id="reviewsWritePopup">
+      <ToastContainer />
+      <div
+        onClick={() => closeReviewsWritePopup()}
+        className="reviewsWritePopup_left"></div>
+      <div className="reviewsWritePopup_right">
+        <div className="reviewsWritePopup_header">
+          <button
+            onClick={() => closeReviewsWritePopup()}
+            className="reviewsWritePopup_logo">
+            <img src="/logo.svg" alt="" />
+          </button>
+          <h1 className="reviewsWritePopup_title">Write a review</h1>
 
-                <select
-                    onChange={(e) => setService(e.target.value)}
-                    className="reviewsWritePopup_name_input"
-                    defaultValue={"0"}
-                >
-                    <option disabled value="0">
-                        select service
-                    </option>
-                    {services?.results.map((item: any) => {
-                        return (
-                            <option key={item.id} value={item.id}>
-                                {item.title}
-                            </option>
-                        );
-                    })}
-                </select>
+          <div className="reviewsWritePopup_header_right">
+            <button
+              onClick={() => setFormSelector("write")}
+              className={
+                formSelector === "write"
+                  ? "reviewsWritePopup_header_btn reviewsWritePopup_header_btn-active"
+                  : "reviewsWritePopup_header_btn"
+              }>
+              review
+            </button>
+            <button
+              onClick={() => setFormSelector("video")}
+              className={
+                formSelector === "video"
+                  ? "reviewsWritePopup_header_btn reviewsWritePopup_header_btn-active"
+                  : "reviewsWritePopup_header_btn"
+              }>
+              Video review
+            </button>
+          </div>
 
-                <div className="reviewsWritePopup_productRating">
-                    {/* <h2 className="reviewsWritePopup_productRating_title">
+          <button
+            onClick={() => closeReviewsWritePopup()}
+            className="reviewsWritePopup_header_closeBtn">
+            X
+          </button>
+        </div>
+
+        <div className="reviewsWritePopup_name">
+          <h2 className="reviewsWritePopup_name_title">Full Name</h2>
+          <input
+            type="text"
+            placeholder="Enter your full name"
+            className="reviewsWritePopup_name_input"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </div>
+
+        <select
+          onChange={(e) => setService(e.target.value)}
+          className="reviewsWritePopup_name_input"
+          defaultValue={"0"}>
+          <option disabled value="0">
+            select service
+          </option>
+          {services?.results.map((item: any) => {
+            return (
+              <option key={item.id} value={item.id}>
+                {item.title}
+              </option>
+            );
+          })}
+        </select>
+
+        <div className="reviewsWritePopup_productRating">
+          {/* <h2 className="reviewsWritePopup_productRating_title">
                         Product Rating
                     </h2> */}
 
-                    {/* <div className="reviewsWritePopup_productRating_item">
+          {/* <div className="reviewsWritePopup_productRating_item">
                         <p className="reviewsWritePopup_productRating_item_text">
                             Features
                         </p>
@@ -244,7 +279,7 @@ const ReviewsWritePopup = ({ services }: any) => {
                             />
                         </div>
                     </div> */}
-                    {/* <div className="reviewsWritePopup_productRating_item">
+          {/* <div className="reviewsWritePopup_productRating_item">
                         <p className="reviewsWritePopup_productRating_item_text">
                             Value
                         </p>
@@ -259,29 +294,29 @@ const ReviewsWritePopup = ({ services }: any) => {
                             />
                         </div>
                     </div> */}
-                </div>
+        </div>
 
-                <div className="reviewsWritePopup_productRating">
-                    <h2 className="reviewsWritePopup_productRating_title">
-                        Overall Rating
-                    </h2>
-                    {/* <p className="reviewsWritePopup_productRating_subtitle">
+        <div className="reviewsWritePopup_productRating">
+          <h2 className="reviewsWritePopup_productRating_title">
+            Overall Rating
+          </h2>
+          {/* <p className="reviewsWritePopup_productRating_subtitle">
                         Determined by averaging the above Product Ratings
                     </p> */}
 
-                    <div className="reviewsWritePopup_productRating_item">
-                        <p className="reviewsWritePopup_productRating_item_text">
-                            Features
-                        </p>
-                        <div className="reviewsWritePopup_productRating_item_right">
-                            <Rating
-                                name="simple-controlled"
-                                value={stars}
-                                size="large"
-                                defaultValue={0}
-                                onChange={(e, value) => setStars(value)}
-                            />
-                            {/* <button
+          <div className="reviewsWritePopup_productRating_item">
+            <p className="reviewsWritePopup_productRating_item_text">
+              Features
+            </p>
+            <div className="reviewsWritePopup_productRating_item_right">
+              <Rating
+                name="simple-controlled"
+                value={stars}
+                size="large"
+                defaultValue={0}
+                onChange={(e, value) => setStars(value)}
+              />
+              {/* <button
                 className={stars > 0 ? "star_btn_point" : ""}
                 onClick={() => setStars(1)}>
                 <img src="/one-star-icon.svg" alt="" />
@@ -306,30 +341,29 @@ const ReviewsWritePopup = ({ services }: any) => {
                 onClick={() => setStars(5)}>
                 <img src="/one-star-icon.svg" alt="" />
               </button> */}
-                        </div>
-                    </div>
-                </div>
-
-                {formSelector === "write" ? (
-                    <WriteForm
-                        review_text={review_text}
-                        setReview_text={setReview_text}
-                    />
-                ) : (
-                    <VideoForm video={video} setVideo={setVideo} />
-                )}
-
-                <p>{message}</p>
-
-                <button
-                    onClick={() => submitReview()}
-                    className="reviewsWritePopup_submitBtn"
-                >
-                    Submit review
-                </button>
             </div>
+          </div>
         </div>
-    );
+
+        {formSelector === "write" ? (
+          <WriteForm
+            review_text={review_text}
+            setReview_text={setReview_text}
+          />
+        ) : (
+          <VideoForm video={video} setVideo={setVideo} />
+        )}
+
+        <p>{message}</p>
+
+        <button
+          onClick={() => submitReview()}
+          className="reviewsWritePopup_submitBtn">
+          Submit review
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ReviewsWritePopup;
--- front/app/reviews/ReviewsPopup/ReviewsWritePopup/VideoForm.tsx
+++ front/app/reviews/ReviewsPopup/ReviewsWritePopup/VideoForm.tsx
@@ -1,40 +1,40 @@
 import DownloadingIcon from "@mui/icons-material/Downloading";
 export default function VideoForm({ video, setVideo }: any) {
-    return (
-        <div className="reviewsWritePopup_bottom">
-            <h1 className="reviewsWritePopup_bottom_title">upload video</h1>
-            <div className="reviewsWritePopup_bottom_img">
-                {video?.name ? (
-                    <div className="mt-10 relative">
-                        <button
-                            onClick={() => {
-                                setVideo("");
-                            }}
-                            className="reviewsWritePopup_bottom_btn"
-                        >
-                            &#10006;
-                        </button>
-                        <img src="/reviews-video-img.jpg" alt="" />
-                        <p>{video.name}</p>
-                    </div>
-                ) : (
-                    <label
-                        htmlFor="fileUpload"
-                        className="add_file cursor-pointer"
-                    >
-                        {/* upload video */}
-                        <DownloadingIcon fontSize="large" />
-                        <input
-                            type="file"
-                            id="fileUpload"
-                            className="fileUpload"
-                            onChange={(e: any) => {
-                                setVideo(e.target.files[0]);
-                            }}
-                        />
-                    </label>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className="reviewsWritePopup_bottom">
+      <h1 className="reviewsWritePopup_bottom_title">upload video</h1>
+      <div className="reviewsWritePopup_bottom_img">
+        {video?.name ? (
+          <div className="mt-10 relative">
+            <button
+              onClick={() => {
+                setVideo("");
+              }}
+              className="reviewsWritePopup_bottom_btn">
+              &#10006;
+            </button>
+            {/* <img src="/reviews-video-img.jpg" alt="" /> */}
+            <video width="400" controls>
+              <source src={URL.createObjectURL(video)} />
+            </video>
+            <p>{video.name}</p>
+          </div>
+        ) : (
+          <label htmlFor="fileUpload" className="add_file cursor-pointer">
+            {/* upload video */}
+            <DownloadingIcon fontSize="large" />
+            <input
+              type="file"
+              id="fileUpload"
+              className="fileUpload"
+              accept="video/mp4,video/x-m4v,video/*"
+              onChange={(e: any) => {
+                setVideo(e.target.files[0]);
+              }}
+            />
+          </label>
+        )}
+      </div>
+    </div>
+  );
 }
