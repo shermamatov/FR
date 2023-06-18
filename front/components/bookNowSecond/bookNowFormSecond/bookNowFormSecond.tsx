6b943ff7c265730cfad3d2e064f5e412a2aad585
DASTAN DOKTURBEKOV <dastanhayama@gmail.com>
2023-06-18 17:27:27 +0600

    booking post v2

--- front/components/bookNowSecond/bookNowFormSecond/bookNowFormSecond.tsx
+++ front/components/bookNowSecond/bookNowFormSecond/bookNowFormSecond.tsx
@@ -177,7 +177,6 @@ export default function BookNowFormSecond({
         headers: {
           "Content-Type": "application/json",
           Accept: " */*",
-          "Accept-Encoding": "gzip, deflate, br",
           Connection: "keep-alive",
         },
       };
